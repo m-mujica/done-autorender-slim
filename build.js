57dba55eaff82394d1eb5e2878babdbffb101454
Manuel Mujica <manuel.expat@gmail.com>
2017-09-28 11:23:10 -0600

    Replicate issue

---
+++ build.js
@@ -0,0 +1,6 @@
+var path = require("path");
+var stealTools = require("steal-tools");
+
+stealTools.optimize({}, {
+  minify: false
+});
---
+++ state.js
@@ -0,0 +1,5 @@
+var CanMap = require("can-map");
+
+module.exports = CanMap.extend({
+  message: "Hello, World!"
+});
